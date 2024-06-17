aaae1ac2c0ebbf0f2e829f983f1f1e9933911e1d
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2024-06-17 23:04:04 +0530

    Add files via upload

---
+++ Assignment 2/thridmax.java
@@ -0,0 +1,24 @@
+public class thridmax {
+
+    public int thirdMax(int[] nums) {
+        long firstMax, secMax, thirdMax;
+        firstMax = secMax = thirdMax = Long.MIN_VALUE;
+
+        for (int n : nums) {
+            if (n > firstMax) {
+                thirdMax = secMax;
+                secMax = firstMax;
+                firstMax = n;
+            } else if (n > secMax && n != firstMax) {
+                thirdMax = secMax;
+                secMax = n;
+            } else if (n > thirdMax && n != firstMax && n != secMax) {
+                thirdMax = n;
+            }
+        }
+        if (thirdMax == Long.MIN_VALUE) {
+            return (int) firstMax;
+        }
+        return (int) thirdMax;
+    }
+}
